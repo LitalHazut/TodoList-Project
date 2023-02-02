a8473a4654271e1c79a006a9470f09c9269b5ab8
lital hazut <lital7h@gmail.com>
2023-02-02 16:32:22 +0200

    add renderlist

--- server/tasksService.js
+++ server/tasksService.js
@@ -1,9 +1,9 @@
-module.exports = class TasksService {
-  getAllTasks() {
-    db.query('SELECT * FROM tasks', (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-};
+// module.exports = class TasksService {
+//   getAllTasks() {
+//     db.query('SELECT * FROM tasks', (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//     });
+//   }
+// };
